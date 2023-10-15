ffedd7f8720e93e40a212adcac66792b2aab4bbb
Manel BELHADJ <belhaj343@gmail.com>
2023-10-15 11:26:10 +0100

    fixed routes

--- user-books/index.js
+++ user-books/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import { getUsers, getUser, addUser, deleteUser } from "./services/users";
-import { getBooks, getBook, addBook, deleteBook } from "./services/books";
+import {
+  getUsers,
+  getUser,
+  addUser,
+  deleteUser,
+} from "./services/users/index.js";
+import {
+  getBooks,
+  getBook,
+  addBook,
+  deleteBook,
+} from "./services/books/index.js";
 
 dotenv.config();
 
@@ -11,16 +21,16 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.use("/users", getUsers);
-app.use("/user", getUser);
-app.use("/add-user", addUser);
-app.use("/delete-user", deleteUser);
+app.get("/users", getUsers);
+app.get("/user/:id", getUser);
+app.post("/users", addUser);
+app.delete("/user/:id", deleteUser);
 
-app.use("/books", getBooks);
-app.use("/book", getBook);
-app.use("/add-book", addBook);
-app.use("/delete-book", deleteBook);
+app.get("/books", getBooks);
+app.get("/book/:id", getBook);
+app.post("/books", addBook);
+app.delete("/book/:id", deleteBook);
 
-app.listen(() => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App started on port ${PORT}`);
 });
